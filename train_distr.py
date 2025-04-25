422c70815dd1530a6abdd6a4538cc948dc219365
mrmorais <maradona.morais@cobli.co>
2025-04-25 19:39:07 -0300

    chore: import torch

--- train_distr.py
+++ train_distr.py
@@ -1,9 +1,10 @@
+import torch
 from ray.train import ScalingConfig
 from ray.train.torch import TorchTrainer
 from train_model import train
 from functools import partial
 
-scaling_config = ScalingConfig(num_workers=2, use_gpu=False)
+scaling_config = ScalingConfig(num_workers=1, use_gpu=False)
 
 trainer = TorchTrainer(partial(train, use_ray=True), scaling_config=scaling_config)
 trainer.fit()
