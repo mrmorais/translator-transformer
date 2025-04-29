3503541f8c5f69afe626ed39eaeb51bc72e0e1d0
mrmorais <maradona.morais@cobli.co>
2025-04-29 19:10:32 -0300

    chore: add pythonpath

--- train_distr.py
+++ train_distr.py
@@ -1,9 +1,12 @@
 import torch
 import ray
+import os
 from ray.train import ScalingConfig
 from ray.train.torch import TorchTrainer
 from train_model import train
 from functools import partial
+
+os.environ['PYTHONPATH'] = '/home/ray/translator-transformer'
 
 ray.init(address="auto")
 scaling_config = ScalingConfig(num_workers=3, use_gpu=False)
