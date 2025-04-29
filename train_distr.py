069eb644d2aaa0534a16fe6cabf0d64cdb6b319b
mrmorais <maradona.morais@cobli.co>
2025-04-29 18:54:21 -0300

    chore: add init

--- train_distr.py
+++ train_distr.py
@@ -1,10 +1,14 @@
 import torch
+import ray
 from ray.train import ScalingConfig
 from ray.train.torch import TorchTrainer
 from train_model import train
 from functools import partial
 
+ray.init(address="auto")
 scaling_config = ScalingConfig(num_workers=3, use_gpu=False)
 
 trainer = TorchTrainer(partial(train, use_ray=True), scaling_config=scaling_config)
 trainer.fit()
+
+ray.kill()
