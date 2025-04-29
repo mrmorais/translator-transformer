1fec3090b49243466b69477b68c92128fb668a1a
mrmorais <maradona.morais@cobli.co>
2025-04-29 18:39:45 -0300

    chore: cpu, 3 workers

--- train_distr.py
+++ train_distr.py
@@ -4,7 +4,7 @@ from ray.train.torch import TorchTrainer
 from train_model import train
 from functools import partial
 
-scaling_config = ScalingConfig(num_workers=2, use_gpu=True)
+scaling_config = ScalingConfig(num_workers=3, use_gpu=False)
 
 trainer = TorchTrainer(partial(train, use_ray=True), scaling_config=scaling_config)
 trainer.fit()
